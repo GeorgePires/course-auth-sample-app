c95f668b682ec33b15d5e4f6ce5454ba60344132
GeorgePires <georgeasp.pi@gmail.com>
2023-07-03 17:54:22 -0300

    :sparkles: feat: add private account

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,4 +2,10 @@ class UserPolicy < ApplicationPolicy
   def update?
     user.id == record.id
   end
+
+  def show_tweets?
+    return true unless record.private
+    return true if user.id == record.id
+    record.received_follows.where(status: "accepted", follower_id: user.id).any?
+  end
 end
